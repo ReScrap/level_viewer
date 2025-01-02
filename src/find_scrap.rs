554d23272fdae11be07c085defe03af1d86acf1a
Daniel Seiller <earthnuker@gmail.com>
2025-01-02 20:07:39 +0100

    Update find_scrap to return errors instead of panicking

--- src/find_scrap.rs
+++ src/find_scrap.rs
@@ -1,10 +1,10 @@
 use std::path::PathBuf;
-
+use anyhow::{anyhow, Result};
 use steamlocate::SteamDir;
 const APP_ID: u32 = 897610;
 
-pub(crate) fn get_path() -> Option<PathBuf> {
-    let mut steam = SteamDir::locate()?;
-    let app = steam.app(&APP_ID)?;
-    Some(app.path.clone())
+pub(crate) fn get_path() -> Result<PathBuf> {
+    let steam = SteamDir::locate()?;
+    let (app,lib) = steam.find_app(APP_ID)?.ok_or_else(|| anyhow!("Scrapland (App ID {APP_ID}) not found in steam library"))?;
+    Ok(lib.resolve_app_dir(&app))
 }
