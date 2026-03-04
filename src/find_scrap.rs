2feb2a4fef5e41d045094c399bbd67aa502d285c
Daniel Seiller <earthnuker@gmail.com>
2026-03-04 17:13:09 +0100

    chore(version): 0.2.0

--- src/find_scrap.rs
+++ src/find_scrap.rs
@@ -1,10 +1,13 @@
 use std::path::PathBuf;
+
 use anyhow::{anyhow, Result};
 use steamlocate::SteamDir;
 const APP_ID: u32 = 897610;
 
 pub(crate) fn get_path() -> Result<PathBuf> {
     let steam = SteamDir::locate()?;
-    let (app,lib) = steam.find_app(APP_ID)?.ok_or_else(|| anyhow!("Scrapland (App ID {APP_ID}) not found in steam library"))?;
+    let (app, lib) = steam
+        .find_app(APP_ID)?
+        .ok_or_else(|| anyhow!("Scrapland (App ID {APP_ID}) not found in steam library"))?;
     Ok(lib.resolve_app_dir(&app))
 }
