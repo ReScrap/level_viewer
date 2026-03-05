a3140c8c08783b3a63dd20cbbb8ec605347314b9
Daniel Seiller <earthnuker@gmail.com>
2026-03-05 08:29:17 +0100

    feat(export): parallelize image compression in exporter

--- src/export.rs
+++ src/export.rs
@@ -1,88 +1,104 @@
+use crate::parser::{Level, ParsedData, multi_pack_fs::MultiPackFS, resolve_dep};
+use bevy::log::tracing::{error, info, warn};
+use bevy::prelude::ResMut;
+use color_eyre::eyre::{Result, bail};
+use crossbeam_channel::{Receiver, Sender};
+use dds::{
+    Format,
+    header::{Header, ParseOptions},
+};
+use flate2::{Compression, Status};
+use image::{DynamicImage, GenericImage, GenericImageView, ImageFormat, Rgba, RgbaImage};
 use std::{
-    collections::{BTreeMap, HashMap, HashSet},
-    default,
-    io::{BufWriter, Write},
+    collections::HashMap,
+    io::{BufWriter, Cursor, Read, Write},
+    path::{Path, PathBuf},
+    sync::Arc,
 };
+use zip::write::SimpleFileOptions;
 
-use bevy::{
-    log::{error, info, tracing_subscriber::registry, warn},
-    mesh::{MeshVertexAttribute, MeshVertexAttributeId, PrimitiveTopology, VertexFormat},
-    pbr::StandardMaterial,
-    prelude::{Result as BevyResult, *},
-    reflect::{TypeRegistration, TypeRegistry, serde::ReflectSerializer},
-};
-use color_eyre::eyre::{Context, Result, anyhow, bail};
-use image::{GenericImageView, Rgba};
-use rhexdump::rhexdump;
-use zip::unstable::write;
+use crate::State;
 
-use crate::{
-    LightmapHandles, LightmapNames, MapNames, MapTex, MaterialName, ScrapMat, ScrapMaterial, State,
-    parser::{NodeData, ParsedData},
-};
+fn compress_image_from_bytes(
+    data: &[u8],
+    ext: &str,
+    quantize: bool,
+    optimize: bool,
+) -> Result<Vec<u8>> {
+    if ext.to_lowercase() == "dds" {
+        return convert_dds_to_png(data, quantize, optimize);
+    }
 
-fn compress_image(img: &Image, quantize: bool, optimize: bool) -> BevyResult<Vec<u8>> {
-    use bevy::render::render_resource::TextureFormat;
-    let mut liq = imagequant::new();
-    liq.set_speed(1)?;
-    liq.set_quality(20, 80)?;
-    liq.set_log_callback(|_, msg| {
-        info!(target:"imagequant","{msg}");
-    });
-    let Some(data) = img.data.as_ref() else {
-        return Err("No texture data!".to_string().into());
+    let format = match ext.to_lowercase().as_str() {
+        "jpg" | "jpeg" => ImageFormat::Jpeg,
+        "png" => ImageFormat::Png,
+        "bmp" => ImageFormat::Bmp,
+        "tga" => ImageFormat::Tga,
+        _ => ImageFormat::Png,
     };
-    let width = img.width();
-    let height = img.height();
-    let img = if img.texture_descriptor.format.is_bcn() {
-        let format = match img.texture_descriptor.format {
-            TextureFormat::Bc1RgbaUnorm | TextureFormat::Bc1RgbaUnormSrgb => texpresso::Format::Bc1,
-            TextureFormat::Bc2RgbaUnorm | TextureFormat::Bc2RgbaUnormSrgb => texpresso::Format::Bc2,
-            TextureFormat::Bc3RgbaUnorm | TextureFormat::Bc3RgbaUnormSrgb => texpresso::Format::Bc3,
-            TextureFormat::Bc4RUnorm | TextureFormat::Bc4RSnorm => texpresso::Format::Bc4,
-            TextureFormat::Bc5RgUnorm | TextureFormat::Bc5RgSnorm => texpresso::Format::Bc5,
-            other => {
-                warn!("Unsupported texture format: {other:?}");
-                return Ok(vec![]);
-            }
-        };
-        let w = width as usize;
-        let h = height as usize;
-        let mut buffer = vec![0u8; w * h * 4];
-        format.decompress(data, w, h, &mut buffer);
-        buffer
-    } else if let Some(img) = img.convert(TextureFormat::Rgba8UnormSrgb) {
-        img.data.unwrap()
-    } else {
-        warn!("Invalid format: {:?}", img.texture_descriptor);
-        return Ok(vec![]);
+
+    let img = match image::load_from_memory_with_format(data, format) {
+        Ok(img) => img,
+        Err(e) => {
+            eprintln!("Failed to decode image {}: {}", ext, e);
+            return Ok(vec![]);
+        }
     };
-    assert_eq!(img.len(), (width * height * 4) as usize);
+    compress_rgba_image(&img, quantize, optimize)
+}
+
+fn convert_dds_to_png(data: &[u8], quantize: bool, optimize: bool) -> Result<Vec<u8>> {
+    let data = Cursor::new(data);
+    let mut decoder = dds::Decoder::new(data).unwrap();
+    let size = decoder.main_size();
+    let mut data = vec![0_u8; size.pixels() as usize * 4];
+    let view = dds::ImageViewMut::new(&mut data, size, dds::ColorFormat::RGBA_U8).unwrap();
+    decoder.read_surface(view)?;
+    let img = RgbaImage::from_raw(size.width, size.height, data).unwrap();
+    let img = DynamicImage::ImageRgba8(img);
+    compress_rgba_image(&img, quantize, optimize)
+}
+
+fn compress_rgba_image(img: &DynamicImage, quantize: bool, optimize: bool) -> Result<Vec<u8>> {
+    let rgba = img.to_rgba8();
+    let (width, height) = rgba.dimensions();
+
     let mut buffer = std::io::Cursor::new(Vec::new());
+
     if quantize {
-        let pixels = img
+        let mut liq = imagequant::new();
+        liq.set_speed(1)?;
+        liq.set_quality(0, 100)?;
+        liq.set_log_callback(|_, msg| {
+            info!("imagequant: {}", msg);
+        });
+
+        let pixels: Vec<_> = rgba
+            .as_raw()
             .chunks(4)
             .map(|w| imagequant::RGBA::from([w[0], w[1], w[2], w[3]]))
-            .collect::<Vec<_>>();
-        let mut img = liq.new_image(pixels, width as usize, height as usize, 0.0)?;
-        let mut res = liq.quantize(&mut img)?;
-        let (palette, pixels) = res.remapped(&mut img)?;
-        let mut quantized_img = image::ImageBuffer::new(width, height);
+            .collect();
+
+        let mut img_q = liq.new_image(pixels, width as usize, height as usize, 0.0)?;
+        let mut res = liq.quantize(&mut img_q)?;
+        let (palette, pixels) = res.remapped(&mut img_q)?;
+
+        let mut quantized_img = RgbaImage::new(width, height);
         for (x, y, pixel) in quantized_img.enumerate_pixels_mut() {
             let idx = (y * width + x) as usize;
             let p = &palette[pixels[idx] as usize];
-            *pixel = image::Rgba([p.r, p.g, p.b, p.a]);
+            *pixel = Rgba([p.r, p.g, p.b, p.a]);
         }
-        quantized_img.write_to(&mut buffer, image::ImageFormat::Png)?;
+
+        quantized_img.write_to(&mut buffer, ImageFormat::Png)?;
     } else {
-        let img = image::ImageBuffer::<Rgba<u8>, _>::from_raw(width, height, img).unwrap();
-        img.write_to(&mut buffer, image::ImageFormat::Png)?;
+        rgba.write_to(&mut buffer, ImageFormat::Png)?;
     }
     if !optimize {
         return Ok(buffer.into_inner());
     }
-    let png_bytes = oxipng::optimize_from_memory(
-        &buffer.into_inner(),
+    let png_bytes = match oxipng::optimize_from_memory(
+        buffer.get_ref(),
         &oxipng::Options {
             deflater: oxipng::Deflater::Zopfli(oxipng::ZopfliOptions {
                 iteration_count: 10.try_into().unwrap(),
@@ -91,190 +107,145 @@ fn compress_image(img: &Image, quantize: bool, optimize: bool) -> BevyResult<Vec
             optimize_alpha: true,
             ..oxipng::Options::default()
         },
-    )
-    .map_err(|e| e.to_string())?;
+    ) {
+        Ok(bytes) => bytes,
+        Err(e) => {
+            eprintln!("oxipng optimization failed: {}", e);
+            return Ok(buffer.into_inner());
+        }
+    };
     return Ok(png_bytes);
 }
 
-fn dump_mesh(mesh: &Mesh) -> BTreeMap<String, Vec<u8>> {
-    let mut ret = BTreeMap::new();
-    if let Some(idx) = mesh.indices() {
-        let (key, data) = match idx {
-            bevy::mesh::Indices::U16(items) => (
-                "IDX16",
-                items
-                    .iter()
-                    .flat_map(|v| v.to_le_bytes())
-                    .collect::<Vec<u8>>(),
-            ),
-            bevy::mesh::Indices::U32(items) => (
-                "IDX32",
-                items
-                    .iter()
-                    .flat_map(|v| v.to_le_bytes())
-                    .collect::<Vec<u8>>(),
-            ),
+fn export_worker(
+    fs: Arc<MultiPackFS>,
+    rx: Receiver<(String, String, bool)>, // (src_fs_path, dst_zip_path, is_lightmap)
+    tx: Sender<(String, Vec<u8>)>,        // (dst_zip_path, png_data)
+) -> Result<()> {
+    let mut buffer = Vec::with_capacity(4 * 1024 * 1024); // reuse across jobs
+    for (src_path, dst_zip_path, is_lightmap) in rx {
+        info!("Processing {} -> {}", src_path, dst_zip_path);
+        let (_, src_ext) = src_path.rsplit_once('.').unwrap_or((&src_path, "png"));
+        let Ok(mut fh) = fs.open_file(&src_path) else {
+            warn!("Failed to open texture: {}", src_path);
+            continue;
         };
-        ret.insert(key.to_owned(), data);
+
+        buffer.clear();
+        if fh.read_to_end(&mut buffer).is_err() {
+            warn!("Failed to rad {}", src_path);
+            continue;
+        }
+
+        let Ok(png_data) = compress_image_from_bytes(&buffer, src_ext, true, true) else {
+            warn!("Failed to compress {}", src_path);
+            continue;
+        };
+
+        if !png_data.is_empty() {
+            let (dst_path, _) = dst_zip_path.rsplit_once('.').unwrap_or((&dst_zip_path, "png"));
+            let dst_path = format!("tex/{dst_path}.png");
+            let _ = tx.send((dst_path, png_data));
+        }
     }
-    for (attr, data) in mesh.attributes() {
-        let data = data.get_bytes();
-        let key = format!("{}.{:?}", attr.name, attr.format);
-        ret.insert(key.to_owned(), data.to_owned());
-    }
-    return ret;
+    Ok(())
 }
 
-fn dump_mat_props(mat: &StandardMaterial) -> BTreeMap<String, serde_json::Value> {
-    let mut mat_props: BTreeMap<String, serde_json::Value> = BTreeMap::default();
-    mat_props.insert("base_color".to_string(), {
-        serde_json::to_value(mat.base_color.to_linear()).unwrap()
-    });
-    mat_props.insert("emissive".to_string(), {
-        serde_json::to_value(mat.emissive).unwrap()
-    });
-    mat_props.insert("depth_bias".to_string(), {
-        serde_json::to_value(mat.depth_bias).unwrap()
-    });
-    mat_props.insert("unlit".to_string(), {
-        serde_json::to_value(mat.unlit).unwrap()
-    });
-    mat_props.insert("alpha_mode".to_string(), {
-        serde_json::to_value(format!("{:?}", mat.alpha_mode)).unwrap()
-    });
-    mat_props
-}
-
-// TODO: export material properties
-pub(crate) fn do_export(
-    mut state: ResMut<State>,
-    images: Res<Assets<Image>>,
-    meshes: Res<Assets<Mesh>>,
-    materials: Res<Assets<ScrapMaterial>>,
-    query: Query<(
-        Entity,
-        &Mesh3d,
-        &Name,
-        &MaterialName,
-        &MapNames,
-        &MapTex,
-        &ScrapMat,
-        Option<&LightmapHandles>,
-        Option<&LightmapNames>,
-    )>,
-    mut commands: Commands,
-    mut exit: MessageWriter<AppExit>,
-) -> BevyResult {
-    state.export = false;
-    let Some(ParsedData::Level(lvl)) = state.data.as_ref() else {
-        state.export = true;
-        return Ok(());
+pub fn export_level(fs: &MultiPackFS, lvl: &Level, output_path: &Path) -> Result<()> {
+    info!("Exporting level to {}", output_path.display());
+    let fs = Arc::new(fs.clone());
+    let ncpus = std::thread::available_parallelism()?.get();
+    info!("Compressing textures with {ncpus} workers");
+    let (tx, rx, handles) = {
+        let fs = fs.clone();
+        let (job_tx, job_rx) = crossbeam_channel::unbounded();
+        let (res_tx, res_rx) = crossbeam_channel::bounded(ncpus);
+        let handles: Vec<_> = (0..ncpus).map(move |_| {
+            let (fs, rx, tx) = (fs.clone(), job_rx.clone(), res_tx.clone());
+            std::thread::spawn(move || export_worker(fs, rx, tx))
+        }).collect();
+        (job_tx, res_rx, handles)
     };
-    // for sm3 in lvl.sm3.iter().flatten() {
-    //     for node in &sm3.scene.nodes {
-    //         if let Some(NodeData::Light(luz)) = node.content.as_ref() {
-    //             dbg!(&luz);
-    //         }
-    //     };
-    // }
-    // std::process::exit(1);
-    // return Ok(());
-    println!("Exporting!");
-    let fh = BufWriter::new(std::fs::File::create("dump.zip")?);
+
+    let fh = BufWriter::new(std::fs::File::create(output_path)?);
     let mut zf = zip::ZipWriter::new(fh);
-    let opts = zip::write::SimpleFileOptions::default();
-    let mut name_idx: BTreeMap<String, u32> = BTreeMap::new();
-    let mut mat_name_count: BTreeMap<String, u32> = BTreeMap::new();
-    let mut used_mats: HashSet<Handle<StandardMaterial>> = HashSet::new();
-    let mut textures_to_export: HashMap<String, (&Handle<Image>, bool)> = HashMap::new();
-    for (
-        _ent,
-        mesh,
-        name,
-        MaterialName(mat_name),
-        MapNames(map_names),
-        MapTex(map_tex),
-        ScrapMat(scrap_map),
-        lm_handles,
-        lm_names,
-    ) in query.iter()
-    {
-        let n: &mut u32 = name_idx.entry(name.to_string()).or_default();
-        *n += 1;
+    let opts = SimpleFileOptions::default();
 
-        // Get unique material name
-        let unique_mat_name = {
-            let count = mat_name_count.entry(mat_name.clone()).or_default();
-            *count += 1;
-            if *count > 1 {
-                format!("{}_{}", mat_name, count)
-            } else {
-                mat_name.clone()
-            }
-        };
-        let map_names = map_names
-            .iter()
-            .map(|(s, n)| (*s, n.clone()))
-            .collect::<HashMap<_, _>>();
-        for (slot, handle) in map_tex.iter() {
-            if let Some(handle) = handle
-                && images.get(handle).is_some()
+    let mut textures_to_export: HashMap<String, (String, bool)> = HashMap::new();
+    for mat in &lvl.emi.materials {
+        for map_opt in &mat.1.maps {
+            if let Some(map) = map_opt.get()
+                && !map.texture.string.is_empty()
+                && let Some(rpath) = lvl.dependencies.get(&map.texture.string)
             {
-                let key = map_names[slot].as_str();
                 textures_to_export
-                    .entry(key.to_owned())
-                    .or_insert_with(|| (handle, false));
+                    .entry(map.texture.string.clone())
+                    .or_insert((rpath.clone(), false));
             }
         }
-        if let (Some(LightmapHandles(h1, h2)), Some(LightmapNames(name1, name2))) =
-            (lm_handles, lm_names)
-        {
-            if let Some(h) = h1.as_ref() {
-                textures_to_export
-                    .entry(name1.clone())
-                    .or_insert_with(|| (h, true));
-                zf.start_file_from_path(format!("obj/{name}_{n}/_lm1", name = name.as_str()), opts)
-                    .unwrap();
-                zf.write_all(name1.as_bytes()).unwrap();
-            }
-            if let Some(h) = h2.as_ref() {
-                textures_to_export
-                    .entry(name2.clone())
-                    .or_insert_with(|| (h, true));
-                zf.start_file_from_path(format!("obj/{name}_{n}/_lm2", name = name.as_str()), opts)
-                    .unwrap();
-                zf.write_all(name2.as_bytes()).unwrap();
-            }
-        }
-        let mesh = meshes.get(mesh).unwrap();
-        zf.start_file_from_path(format!("obj/{name}_{n}/_mat", name = name.as_str()), opts)
-            .unwrap();
-        zf.write_all(unique_mat_name.as_bytes()).unwrap();
-        for (k, v) in dump_mesh(mesh) {
-            zf.start_file_from_path(format!("obj/{name}_{n}/{k}", name = name.as_str()), opts)
-                .unwrap();
-            zf.write_all(&v).unwrap();
-        }
-        zf.start_file_from_path(format!("mat/{name}.json", name = unique_mat_name), opts)
-            .unwrap();
-        let mat_json = serde_json::to_string_pretty(scrap_map).unwrap();
-        zf.write_all(mat_json.as_bytes()).unwrap();
     }
 
-    // Finally, export all collected textures
-    for (path, (handle, is_lightmap)) in textures_to_export {
-        if let Some(img) = images.get(handle)
-            && let Ok(data) = compress_image(img, !is_lightmap, false)
-            && !data.is_empty()
-        {
-            let file_path = if is_lightmap {
-                format!("lightmaps/{path}.png")
-            } else {
-                format!("tex/{path}.png")
-            };
-            zf.start_file_from_path(&file_path, opts).unwrap();
-            zf.write_all(&data).unwrap();
+    for lm in &lvl.emi.maps {
+        if let Some((key_1, _, key_2)) = &lm.data {
+            for k in &[key_1, key_2] {
+                if let Some(rpath) = lvl.dependencies.get(&k.string) {
+                    textures_to_export
+                        .entry(k.string.clone())
+                        .or_insert((rpath.clone(), false));
+                }
+            }
         }
     }
-    return Ok(());
+
+    zf.start_file("textures.json.gz", opts)?;
+    let mut comp = flate2::write::GzEncoder::new(Vec::new(), Compression::best());
+    serde_json::to_writer(&mut comp, &textures_to_export)?;
+    let data = comp.finish()?;
+    zf.write_all(&data)?;
+
+    info!("Exporting: {} textures", textures_to_export.len());
+    for (dst_path, (src_path, is_lightmap)) in textures_to_export {
+        tx.send((src_path, dst_path, is_lightmap))?;
+    }
+    drop(tx);
+    zf.start_file("level.json.gz", opts)?;
+    let mut comp = flate2::write::GzEncoder::new(Vec::new(), Compression::best());
+    serde_json::to_writer(&mut comp, lvl)?;
+    let data = comp.finish()?;
+    zf.write_all(&data)?;
+
+    for (dst_path, png_data) in rx.iter() {
+        if png_data.is_empty() {
+            continue;
+        }
+        zf.start_file(&dst_path, opts)?;
+        zf.write_all(&png_data)?;
+        info!("wrote {}", dst_path);
+    }
+
+    for h in handles {
+        h.join().unwrap().unwrap();
+    }
+    zf.finish()?;
+    println!("Export complete!");
+    Ok(())
+}
+
+pub(crate) fn do_export(mut state: ResMut<State>) -> bevy::prelude::Result {
+    state.export = false;
+
+    let fs = &state.fs;
+    let lvl: &Level = match state.data.as_ref() {
+        Some(ParsedData::Level(l)) => &l,
+        _ => {
+            return Ok(());
+        }
+    };
+
+    let out_path = PathBuf::from("dump.zip");
+
+    if let Err(e) = export_level(fs, lvl, &out_path) {
+        eprintln!("Export failed: {}", e);
+    }
+    Ok(())
 }
